ac4db10233769bcb0e83b384b73d57bde3d4a862
Vlad <vladislavbarinov537@gmail.com>
2022-01-05 17:41:30 +0200

    script

---
+++ script.js
@@ -0,0 +1,151 @@
+// Hello world!
+
+let firstVariable;
+
+firstVariable = 'Hello world!';
+
+console.log(firstVariable);
+
+let userName = 'Vlad';
+console.log(userName)
+
+let userSurname = 'Barinov';
+console.log(userSurname)
+
+// let width, height, length;
+
+// let maxSpeed = 200
+//   minSpeed = 40
+
+// let button = 'button'
+// let form;
+// let input;
+
+let myHobby = 'walking';
+
+console.log(myHobby);
+
+let myAge = 18;
+// ..
+myAge = 18
+console.log(myAge);
+
+let shoppingList = 'eggs, milk, potatoes ';
+console.log(shoppingList)
+
+const isUserOnline = true;
+const hasPermission = false;
+
+// let rectangleWidth = 200;
+// let rectangleHeight = rectangleWidth;
+
+// // let user;
+// // let userSurname;
+// let dinosaurAge;
+// let info1;
+
+// let operationResult;
+// let operationresult;
+
+// let $user;
+// let _log;
+
+const clientNickname = 'user42';
+
+// const PI = 3.14;
+// const RED_COLOR = '#FF0000';
+
+// var oldBoy = 'Jack';
+// oldBoy = 'Peter';
+
+// console.log(oldBoy)
+
+// let clientAge = 42;
+
+// if (true){
+//     let a = 10;
+// }
+// if (true){
+//     var b = 12;
+// }
+
+// console.log(b)
+
+// function example(){
+//     var c=1;
+// }
+
+// console.log(c);
+
+// console.log(typeof 123);
+// console.log(typeof 'test');
+
+
+// let example;
+// console.log('declare', typeof example)
+
+// example = 10;
+// console.log('init with number', typeof example)
+
+// example = 'abc';
+// console.log('init with string', typeof example)
+
+// const number = 12;
+// console.log('type of number', typeof number)
+
+// const decimalNumber = 0.01;
+// const PI = 3.14;
+
+// const num1 = 1;
+// const num2 = 2;
+
+// const result = num1 + num2;
+// console.log('result', result)
+
+// const string = 'Hello';
+// console.log('typeof string', typeof string);
+
+// const greeting = 'Hello';
+// const space = ' ';
+// const name = 'Ann';
+
+// const fullGreeting = greeting + space + name;
+// console.log('fullGreeting', fullGreeting);
+
+
+
+// const comparisonResult = 5 > 3;
+// console.log('comparisonResult', comparisonResult);
+
+// let pointsCount = 100;
+
+// console.log('pointsCount', pointsCount);
+// console.log('typeof pointsCount', typeof pointsCount);
+
+// pointsCount = String(pointsCount);
+// console.log('pointsCount', pointsCount);
+// console.log('typeof pointsCount', typeof pointsCount);
+
+let employeeSalary = 1000;
+
+console.log('employeeSalary', employeeSalary)
+
+const zeros = '.00';
+const money = '$';
+const result = String(employeeSalary) + zeros + money;
+
+console.log('result', result)
+
+let mouseCoordinates = 34355.675;
+
+console.log('mouseCoordinates', mouseCoordinates)
+
+// mouseCoordinates - Переменная обявлена прямо в консоли
+
+// let value = '42';
+// console.log('value', value);
+// console.log('typeof value', typeof value);
+
+// value = +value;
+// console.log('value', value);
+// console.log('typeof value', typeof value);
---
+++ script2.js
@@ -0,0 +1,117 @@
+// alert('Hello');
+// alert('42');
+
+// const winMessage = 'Congratulations! You won!';
+
+// alert(winMessage);
+
+// const userNameAlert = prompt ('What is your name?');
+// console.log('userNameAlert', userNameAlert);
+
+// 
+// const isConfirmed = confirm(
+//     'Are you really ${userNameAlert} ?'
+//     );
+// console.log('isConfirmed', isConfirmed);
+// 
+
+// if (isConfirmed){
+//     alert('Your data saved!');
+// }
+
+// alert ('Hello ' + userNameAlert + '!');
+
+// const isAdmin = confirm('Are you admin?');
+// console.log('isAdmin', isAdmin);
+
+// if (isAdmin){
+//     alert('Welcome!');
+// }
+
+// let userMoneyAmount = 500;
+// const amountToAdd = Number(prompt('How much to add?'));
+// userMoneyAmount = userMoneyAmount + amountToAdd;
+
+// alert('Now in your wallet: ' + userMoneyAmount);
+
+const num1 = 13;
+const num2 = 21;
+let operationResult;
+
+operationResult = num1 + num2;
+console.log('13+21=', operationResult);
+
+let x=3;
+
+console.log('x++', x++);
+console.log('main x', x);
+
+let resultNumber;
+
+resultNumber = 5-10*3;
+console.log('5-10*3', resultNumber);
+
+let a=10;
+let b=12;
+
+a += b;
+console.log('a', a);
+
+const userNameIf = prompt('Как Вас зовут?');
+
+if(!userNameIf){
+    alert('Вы не ввели имя!');
+}
+
+// задание 1
+ 
+// const userAnsver1 = Number(prompt('Первое число'));
+// const userAnsver2 = Number(prompt('Второе число'));
+// if(userAnsver1 > userAnsver2) {
+//     alert ('Число ' + userAnsver1 + ' больше чем число ' + userAnsver2);
+// } if(userAnsver1 < userAnsver2) {
+//     alert ('Число ' + userAnsver2 + ' больше чем число ' + userAnsver1);
+// }
+//  else {
+//     alert ('Числа равны' );
+// }
+
+//  задание 2
+
+// const userAnsver1 = Number(prompt('Первое число'));
+// const userAnsver2 = Number(prompt('Второе число'));
+// const userAction = (prompt('Укажите действие'));
+
+// switch (userAction) {
+//     case '+':
+//         alert(userAnsver1 + userAnsver2);
+//         break;
+//     case '-':
+//         alert(userAnsver1 - userAnsver2);
+//         break;
+//     case '*':
+//         alert(userAnsver1 * userAnsver2);
+//         break;
+//     case '/':
+//         alert(userAnsver1 / userAnsver2);
+//         break;
+//     default:
+//         alert('Такого действия нет');
+// }
+
+// задание 3
+
+const userAge = (prompt('Укажите свой возраст'));
+const userParents =  (confirm ('Вы со взрослыми?'));
+
+if (userAge > 17 &&  userAge < 60 ){
+     alert('Добро пожаловать');
+} else if (userParents) {
+    alert('Добро Пожаловать!');
+} else{
+    alert('Доступ закрыт.');
+}
+
+     
+
+
