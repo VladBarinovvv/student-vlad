b8906d78aed6a899b65b413ff8ed8a930343fafe
Vlad <vladislavbarinov537@gmail.com>
2022-01-21 10:57:36 +0200

    hw8

---
+++ script-hw8.js
@@ -0,0 +1,52 @@
+// const flat = (arr, res = []) => {
+//     // const res = [];
+  
+//     for (let elem of arr) {
+//       if (Array.isArray(elem)) {
+//         flat(elem, res);
+//       } else {
+//         res.push(elem);
+//       }
+//     }
+  
+//     return res;
+//   };
+  
+//   // const flatArray = flat([[1, 2], [3, 4], 5, [6, 7, 8]]);
+//   const flatArray = flat([[1, 2], [3, 4, [11, 12]], 5, [6, 7, 8]]);
+  
+//   console.log(`flatArray`, flatArray);
+
+  // задание 1
+
+//   const getMean = (arr) =>{
+//       let meanTrue = 0;
+     
+//       for (let i = 0; i < arr.length; i++){
+//         if (arr === true){
+//             meanTrue += 1;
+//         }
+//         return meanTrue;
+//      }
+//   };
+
+//   const number = getMean([true, false, false, true]);
+//   console.log(number);
+
+  
+  const getMean = (arr) =>{
+    let meanTrue = 0;
+
+    for (let item of arr){
+        console.log(item);
+      if (item === true){
+          meanTrue += 1;
+      }
+      return meanTrue;
+      
+   }
+
+};
+
+const number = getMean([ false, false, true]);
+console.log(number);
