78ca86ca1872ff71e410145a0ca5c83a217df02e
Vlad <vladislavbarinov537@gmail.com>
2022-02-04 18:35:31 +0200

    hw8

--- script-hw8.js
+++ script-hw8.js
@@ -20,32 +20,68 @@
   // задание 1
 
 //   const getMean = (arr) =>{
-//       let meanTrue = 0;
-     
-//       for (let i = 0; i < arr.length; i++){
-//         if (arr === true){
-//             meanTrue += 1;
-//         }
-//         return meanTrue;
-//      }
-//   };
 
-//   const number = getMean([true, false, false, true]);
-//   console.log(number);
+//     let meanTrue = 0;
 
-  
-  const getMean = (arr) =>{
+//     for (let item of arr){
+//         // console.log(item);
+//       if (item === true){
+//           meanTrue += 1;
+//       }    
+//    }
+//    return meanTrue;
+// };
 
-    let meanTrue = 0;
+// const number = getMean([true, false, false, true, true]);
+// console.log(number);
 
-    for (let item of arr){
-        // console.log(item);
-      if (item === true){
-          meanTrue += 1;
-      }    
-   }
-   return meanTrue;
+// задание 2
+
+const getOccurrencesCount = (arr) => {
+  const res = {};
+
+  for (let item of arr) {
+    
+
+    if (!res[item]) {
+      res[item] = 1;
+    } else {
+      res[item] += 1;
+    }
+  }
+
+  return res;
 };
 
-const number = getMean([true, false, false, true, true]);
-console.log(number);
+let result = getOccurrencesCount(["a","f","f","s","f","a"])
+console.log (result);
+  
+// задание 3
+
+// function numbers(arr) {
+//   let j = false;
+//   let d = false;
+//   if (((arr[0] % 2 === 0) && (arr[1] % 2 === 0)) || ((arr[1] % 2 === 0) && (arr[2] % 2 === 0))) {
+//     d = true;
+//   } else {
+//     j = true;
+//   }
+
+//   if (j) {
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] % 2 === 0) {
+//         return arr[i];
+//       }
+//     }
+//   } else if (d) {
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] % 2 !== 0) {
+//         return arr[i];
+//       }
+//     }
+//   }
+// }
+
+// let result = numbers([5,7,9,2,3,13] );
+// console.log(result);
+
