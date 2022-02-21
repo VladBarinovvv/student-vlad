090c4a9bb08c0316f31e452c4e3d66a0ddda73bf
Vlad <vladislavbarinov537@gmail.com>
2022-02-21 17:55:17 +0200

    hw13

---
+++ lesson13/script.js
@@ -0,0 +1,28 @@
+
+let slides = document.querySelectorAll('#slides .slide');
+let currentSlide = 0;
+
+function nextSlide(){
+	goToSlide(currentSlide+1);
+}
+
+function previousSlide(){
+	goToSlide(currentSlide-1);
+}
+
+function goToSlide(e){
+	slides[currentSlide].className = 'slide';
+	currentSlide = (e+slides.length)%slides.length;
+	slides[currentSlide].className = 'slide showing';
+}
+
+let next = document.getElementById('next');
+let previous = document.getElementById('previous');
+
+next.onclick = function(){
+		nextSlide();
+};
+previous.onclick = function(){
+	previousSlide();
+};
+
